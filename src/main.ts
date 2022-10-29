af48c369c88b5075b60327b2441ef2e0564b7b89
Adalid Claure <aclaure@gmail.com>
2022-10-28 21:15:35 -0400

    subscribes and receives messages;

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
 import * as path from "path";
+import * as mqtt from 'mqtt';
 
 const createWindow = (): void => 
 {
@@ -7,18 +8,18 @@ const createWindow = (): void =>
     {
         width: 800,
         height: 600,
-        titleBarStyle: 'hidden',
+        // titleBarStyle: 'hidden',
         title: "ElectricOwl!",
         webPreferences:
         {
-            worldSafeExecuteJavaScript: true,
+            // worldSafeExecuteJavaScript: true,
             contextIsolation: true,
             nodeIntegration: false,
             preload: path.join(__dirname, "preload.js"),
         },
 
         // @ts-ignore: This gives an error on Windows, but it works fine.
-        titleBarOverlay: true,
+        // titleBarOverlay: true,
     });
 
     win.loadFile('../src/index.html');
@@ -53,6 +54,27 @@ app.on("window-all-closed", () =>
 
 ipcMain.on("connectbtn-clicked", (event, data) =>
 {
-    console.log("event received!");
-    event.reply("msg-from-main", "Hello from main!");
+    let client = mqtt.connect('mqtt://localhost:9099');
+    // console.log(client);
+    // console.log("event received!");
+    // event.reply("msg-from-main", "Hello from main!");
+
+    client.on('connect', () =>
+    {
+        console.log("Connected to MQTT broker!");
+        event.reply("msg-from-main", "Connected to MQTT from main!!");
+
+        client.subscribe('test', function() 
+        {
+            console.log('Subscribed');
+        });
+    });
+
+    client.on('message', function (topic:string, message:string) 
+    {
+        // message is Buffer
+        console.log("topic: " + topic);
+        console.log("message: " + message.toString());
+        client.end()
+    });
 });
--- src/preload.ts
+++ src/preload.ts
@@ -1,11 +1,7 @@
 import { contextBridge, ipcMain, ipcRenderer, app } from 'electron';
 
-const process = require('process');
-console.log("Current working directory: ", process.cwd());
-
 // https://github.com/electron/electron/issues/35587
 // import * as appversion from 'appversion';
-
 // contextBridge.exposeInMainWorld('appversion', appversion);
 
 let sendSubmit = (data: any) =>
@@ -20,46 +16,7 @@ let sendSubmit = (data: any) =>
 
 let indexBridge = 
 {
-    sendSubmit: sendSubmit
+    sendSubmit: sendSubmit,
 };
 
 contextBridge.exposeInMainWorld("Bridge", indexBridge);
-
-// contextBridge.exposeInMainWorld('versions', { value : "1.1.2" });
-// 
-// console.log(appversion.version());
-
-// ipcRenderer.addListener("connectbtn-clicked", () =>
-// {
-//     console.log("connectbtn-clicked received");
-// });
-
-// const currentWindowsPID3 = remote.getCurrentWindow().id;
-// console.log("currentWindowsPID3: " + currentWindowsPID3);
-// let procId = remote.getCurrentWebContents().getOSProcessId();
-// console.log()
-
-// const mqtt = require('mqtt')
-// import * as mqtt from "mqtt"
-// (window as any).mqtt = mqtt
-// console.log("preload-mqtt: " + mqtt)
-// contextBridge.exposeInMainWorld('mqtt', mqtt);
-
-
-// const options = {
-//     // Clean session
-//     clean: true,
-//     connectTimeout: 4000,
-//     // Auth
-//     clientId: 'emqx_test',
-//     username: 'emqx_test',
-//     password: 'emqx_test',
-//   }
-//   const client  = mqtt.connect('mqtt://broker.emqx.io:1883', options)
-// client.on('connect', function () {
-//     console.log('Connected')
-//     client.subscribe('test', function() 
-//     {
-//       console.log('Subscribed')
-//     })
-//   })
